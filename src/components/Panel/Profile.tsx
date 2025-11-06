4d32ce997220a6983c2c1d8bcbd6adc286ae0ab9
Behrad <behradbaniadami58@gmail.com>
2025-11-06 15:52:44 +0330

    panel

---
+++ src/components/Panel/Profile.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { useState, ChangeEvent } from "react";
+import { Card, CardHeader, CardBody, CardFooter } from "@heroui/card";
+import { Camera, X } from "lucide-react";
+
+export default function Profile() {
+  return <div></div>;
+}
